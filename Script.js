b8bb44e26535a502229f27cf3b48ad3ebba74638
Hannes <hannesbirgersson@gmail.com>
2015-12-28 22:22:01 +0100

    Cookies

--- Script.js
+++ Script.js
@@ -1,33 +1,48 @@
-
+/*Globala variabler*/
 var newMvs = 0;
 var oldList = 0;
 var newList = 0;
 
+
+
+
 function refreshPage(type) {
+    /*Kollar om några nya inlägg har lagts till och uppdaterar sidan asynkront ifall så är fallet*/
     $.get("ajaxMV.php", function (data) {
         var newList = (data.match(/<li/g) || []).length;
-
+        setCookie("MVAmount", newList, 50);
         if (newList - oldList > 0) {
+            $("#MVs").html(data);
+            
+            /*Här hanteras uppdateringsmeddelande för nya inlägg*/
             if (oldList != 0 && type != "add" && document.hasFocus() == false) {
-                /*Alert goes here*/
-
-                console.log(newList - oldList);
 
                 newMvs = newMvs + (newList - oldList);
                 document.title = "(" + newMvs + ") Minns vi den gången Zahabe";
+
+                for (i = 1; i <= newMvs; i++) {
+                    $("#MVs li:nth-child(" + i + ")").css('background-color', '#f1f1f1');
+                }
             }
-            $("#MVs").html(data);
             oldList = newList;
         }
     });
 }
+
 $(window).focus(function() {
+    /*Tar bort uppdateringsmeddelanden när sidan får fokus*/
     document.title = "Minns vi den gången Zahabe";
     newMvs = 0;
 });
-$(document).ready(function() {
-    $("#MVform").submit(function(e) {
 
+$(window).blur(function () {
+    /*Tar bort uppdateringsmarkeringar när sidan tappar fokus*/
+    $( "#MVs li" ).css('background-color', 'transparent');
+});
+
+$(document).ready(function() {
+    /*Lägger till nya MVs asynkront*/
+    $("#MVform").submit(function(e) {
         var url = "add.php";
 
         $.ajax({
@@ -39,6 +54,7 @@ $(document).ready(function() {
                 $('#errorspace').html("");
                 refreshPage("add");
             },
+            /*Visar ett felmeddelande beroende på vilken HTTP-statuskod skickas tillbaka*/
             error: function(XMLHttpRequest, textStatus, errorThrown) {
                 switch (errorThrown) {
                     case "Not Acceptable":
@@ -62,7 +78,33 @@ $(document).ready(function() {
     });
 });
 
-$(window).load(function() {
-    //refreshPage("");
+$(window).load(function () {
+    /*Init*/
+    oldList = document.getElementById("MVs").getElementsByTagName("li").length;
+    var cookiedList = getCookie("MVAmount");
+
+    for (i = 1; i <= oldList - cookiedList; i++) {
+        $("#MVs li:nth-child(" + i + ")").css('background-color', '#f1f1f1');
+    }
+
     setInterval("refreshPage('')", 5000);
 });
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
